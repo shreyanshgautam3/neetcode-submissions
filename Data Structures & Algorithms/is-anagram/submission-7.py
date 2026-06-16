0a6a8b0d5ad09531232034bc133387ade1f83032
Shreyansh Gautam <255657377+shreyanshgautam3@users.noreply.github.com>
2026-06-16 13:54:43 +0530

    Remove: is-anagram - submission-7

--- Data Structures & Algorithms/is-anagram/submission-7.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        a = collections.Counter(s)
-        b = collections.Counter(t)
-        if a == b:
-            if len(s) == len(t):
-                return True
-        return False
